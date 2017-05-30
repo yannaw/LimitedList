72ff26ce195cad3fdc5c1e8e6058c57672ffc119
yannaw <yannaw@hotmail.com>
2017-05-30 15:22:55 +0200

    Add project files.

---
+++ LimitedList/LimitedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitedList {
+    /// <summary>
+    /// Skulle ha varit Array pga Garage-övningen. Funkar som linked-list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    class LimitedList<T> : IEnumerable<T> {
+        private List<T> items;
+
+        public int Capacity { get; }
+        public int Count => items.Count; // samma som "return get items.count"
+
+        public LimitedList(int capacity) {
+            Capacity = capacity;
+            items = new List<T>();
+        }
+
+        public bool Add(T item) {
+            if(items.Count<Capacity) {
+                items.Add(item);
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            foreach(var item in items) {
+                yield return item; //yield returnerar ett object i taget i en foreach-loop. Mao, den stannar kvar i loppen så länge som det finns element kvar.
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LimitedList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitedList {
+    class Program {
+        static void Main(string[] args) {
+
+
+            var values = GetSomeValues(); // returnerar enumerator
+            
+            foreach(var item in values) {
+                Console.WriteLine(item);
+            }
+            return;
+
+            var list = new LimitedList<string>(6);
+            Console.WriteLine(list.Add("111"));
+            list.Add("222");
+            list.Add("333");
+
+            //           Console.WriteLine(list[1]);
+
+            Inventory(list); //Ctr+R Ctr+M
+
+            Console.WriteLine(list.Add("444"));
+            Inventory(list);
+
+
+        }
+
+        private static void Inventory(LimitedList<string> list) {
+            Console.WriteLine("Innehåll ({0})", list.Count);
+            foreach(var item in list) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable<int> GetSomeValues() {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+        }
+    }
+}
